a0a654f0325cebb95b3f7d90c58943dfd3882066
satya-lakshmi <gvsslakshmi1998@gmail.com>
2021-01-29 11:45:09 +0530

    heroku2 commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ request({ url:url, json:true }, (error, response) => {
       callback('Unable able to find location', undefined);
     }
     else {
-      callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.'); 
+      callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out. The humidity is ' + response.body.current.humidity + '%'); 
     }
   })         
 }
